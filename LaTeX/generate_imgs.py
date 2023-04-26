2fea2f16ec7e40438f94c1ff649e4b05ec67cb00
Alberto Rodriguez Fernandez <17274024+AlbertoRFer@users.noreply.github.com>
2023-04-26 22:39:44 +0200

    Added ontology file and script to manually generate images
    The images are png files with transparent background for all variables and equations.

---
+++ LaTeX/generate_imgs.py
@@ -0,0 +1,37 @@
+import os
+import subprocess
+
+def latex2image(latex_code, output_file):
+    with open(output_file + ".tex", "w") as f:
+        f.write("\\documentclass[border=1pt]{standalone}\n")
+        f.write("\\usepackage{amsmath}\n")
+        f.write("\\begin{document}\n")
+        f.write(latex_code)
+        f.write("\\end{document}\n")
+
+    subprocess.run(["latex", "-interaction=nonstopmode", output_file + ".tex"],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["dvipng", "-D", "150", "-T", "tight", "-z", "9",
+                    "-bg", "Transparent", "-o", output_file + ".png", output_file + ".dvi"],
+                   stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    os.remove(output_file + ".tex")
+    os.remove(output_file + ".aux")
+    os.remove(output_file + ".log")
+    os.remove(output_file + ".dvi")
+    
+variable_dict = {}
+with open("variables.txt", "r") as f:
+    for i, line in enumerate(f):
+        key = "V_" + str(i+1)
+        value = line.strip()
+        variable_dict[key] = value
+
+with open("equations.txt", "r") as f:
+    for i, line in enumerate(f):
+        key = "E_" + str(i+1)
+        value = line.strip()
+        variable_dict[key] = value
+        
+for output, code in variable_dict.items():
+    latex2image(code, output)
